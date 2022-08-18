fde5b6297cbd88cb2fc3d5171efcb32ebf0d95ef
Ding Tian Ding <dingtian.ding@gmail.com>
2022-08-18 16:11:36 -0400

    Time: 260 ms (62.25%), Space: 50.2 MB (92.84%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0)  return false
+    
+    let reversed = 0 
+    
+    for (let i = x; i >= 1; i = Math.floor(i/10)) reversed = rev * 10 + i % 10
+    
+    return reversed === x
+    
+    
+    
+};
+
+var isPalindrome = function(x) {
+  if (x < 0) return false
+
+  let rev = 0
+  for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
+  return rev === x
+};
