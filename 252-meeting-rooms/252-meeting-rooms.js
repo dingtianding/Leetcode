36a20fecffa31b796d2fcc608548e54fade5906d
Ding Tian Ding <dingtian.ding@gmail.com>
2022-08-25 17:36:55 -0400

    Time: 100 ms (51.33%), Space: 44.2 MB (62.30%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -3,7 +3,6 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-    
     let sorted = [...intervals].sort((a, b)=> a[0] - b[0])
     
     for (let i = 0; i < sorted.length - 1; i++) {
