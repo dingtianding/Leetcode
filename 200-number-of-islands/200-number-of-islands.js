1bda9423a2064237c9eea28fb319d315c086651d
Ding Tian Ding <dingtian.ding@gmail.com>
2022-08-25 20:36:28 -0400

    Time: 134 ms (49.29%), Space: 45.1 MB (65.96%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,48 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+
+
+
+var numIslands = function(grid) {
+    let count = 0
+    
+    for(let row = 0; row < grid.length; row++){
+        for(let col = 0; col < grid[row].length; col++){
+            if(grid[row][col] == '1'){
+                count ++
+                explore(row, col, grid)
+            }
+        }
+    }
+    return count
+};
+
+
+function explore(row, col, grid){
+    
+    if(row < 0 || 
+       col < 0 || 
+       row >= grid.length || 
+       col >= grid[row].length || 
+       grid[row][col] === '0'||
+       grid[row][col] === '2'){
+        return 
+    }
+    // 0 = real water
+    // 1 = 1st land
+    // 2 = connected lands
+    
+    grid[row][col] = '2'
+    // if 1 is next 1 it is part of the same island can be treated as 0(non-island)
+    explore(row, col+1, grid) 
+    explore(row, col-1, grid)
+    explore(row+1, col, grid)
+    explore(row-1, col, grid)
+}
+
+// 1. PDF Sign 
+// 2. Instruction on Payee(Bank Check),Take a picture
+// 3. Post office overnight to Nancy Office afternoon
