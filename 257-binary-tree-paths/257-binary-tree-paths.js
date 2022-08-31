ec59f114ac70796214d9cfd17b2e5a44f33d8ec7
Ding Tian Ding <dingtian.ding@gmail.com>
2022-08-31 14:12:48 -0400

    Time: 121 ms (12.24%), Space: 43.5 MB (89.80%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    
+    let paths = [];
+    
+    function dfs(root, cur){
+        if(!root) return;
+        if(!root.left && !root.right) {
+            paths.push(cur + root.val);
+            return
+        }
+        dfs(root.left, cur + root.val + "->");
+        dfs(root.right, cur + root.val + "->");
+    }
+
+
+    dfs(root, "");
+    return paths;
+    
+    
+};
