1d9ad452f755588853af5b608c1cf8d61e61d5da
Ding Tian Ding <dingtian.ding@gmail.com>
2022-09-08 23:27:14 -0400

    Time: 124 ms (20.33%), Space: 44.5 MB (29.59%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -3,17 +3,12 @@
  * @param {number} n
  * @return {number[]}
  */
-var shuffle = function(nums, n) {
-    start = 0
-    midpoint = nums.length / 2
-    
+var shuffle = function(nums, n) {    
     shuffled = []
     
-    while (start < nums.length / 2 && midpoint < nums.length) {
-        shuffled.push(nums[start])
-        shuffled.push(nums[midpoint])
-        start++
-        midpoint++
+    for (i = 0; i < n; i++) {
+        shuffled.push(nums[i])
+        shuffled.push(nums[i+n])
     }
     
     return shuffled
