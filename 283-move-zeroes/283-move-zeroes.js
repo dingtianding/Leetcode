495aa7be4e1af9a7860bbebb51456acef21067b7
Ding Tian Ding <dingtian.ding@gmail.com>
2022-09-09 02:21:23 -0400

    Time: 164 ms (27.33%), Space: 47.2 MB (13.70%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,20 +2,31 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    var pos = 0;
+// var moveZeroes = function(nums) {
+//     var pos = 0;
     
-    for (var i = 0; i < nums.length; i++){
-        num = nums[i]
+//     for (var i = 0; i < nums.length; i++){
+//         num = nums[i]
         
-        if (num !== 0) { // current number is not 0
-            nums[pos] = num// set up the found non-zero. number to the pointer, and then increase it
-            pos++
+//         if (num !== 0) { // current number is not 0
+//             nums[pos] = num// set up the found non-zero. number to the pointer, and then increase it
+//             pos++
+//         }
+//     }
+    
+//     for (i = pos; i < nums.length; i++){
+//         nums[i] = 0;
+//     }
+    
+// };
+
+var moveZeroes = function(nums) {
+    let pointer = 0
+    for (let i in nums){
+        if (nums[i] !== 0){
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]]
+            pointer++
         }
     }
-    
-    for (i = pos; i < nums.length; i++){
-        nums[i] = 0;
-    }
-    
+    return nums
 };
