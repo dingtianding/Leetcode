fef4d739ca50c2261e3b3d0bfc7bf5036fda28ae
Ding Tian Ding <dingtian.ding@gmail.com>
2022-09-09 01:22:03 -0400

    Time: 95 ms (57.57%), Space: 41.7 MB (69.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -6,6 +6,14 @@ var fib = function(n) {
     
     if(n <= 1) return n
     
-    return fib(n - 1) + fib (n - 2)
+    // return fib(n - 1) + fib (n - 2)
+    
+    if (n < 2) return n
+    
+    let a = 0, b = 1
+    
+    for (let i = 1; i < n; i++)
+        [a , b] = [b , a + b]
+    return b
     
 };
