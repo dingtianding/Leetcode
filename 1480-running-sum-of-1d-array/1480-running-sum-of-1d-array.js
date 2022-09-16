9e89cf2b6cb1fbfcdc8aafb028e05e2d30f4610d
Ding Tian Ding <dingtian.ding@gmail.com>
2022-09-15 22:44:09 -0400

    Time: 103 ms (32.75%), Space: 42.8 MB (20.84%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,11 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    for (let i = 1; i < nums.length; ++i){
-        nums[i] += nums[i - 1];
-    }
+// var runningSum = function(nums) {
+//     for (let i = 1; i < nums.length; ++i){
+//         nums[i] += nums[i - 1];
+//     }
     
+//     return nums
+    
+// };
+
+const runningSum = nums => {
+    nums.reduce((a,c,i,arr) => arr[i] += a)
     return nums
-    
-};
+}
