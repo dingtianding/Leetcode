25d2a1e2c968e0a6dcd3ebad2ed70045e0df26c2
Ding Tian Ding <dingtian.ding@gmail.com>
2022-11-21 18:25:53 -0500

    Time: 62 ms (93.13%), Space: 41.7 MB (77.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,62 +2,27 @@
  * @param {number} n
  * @return {number}
  */
-
-// 8======D
-
-//create an array that contains the step you are going to take
-// total has to be = to the n
-// we either reset/add ar change the array to find out all the possbility
-// 1 : 1     [1]
-// 2 : 2     1,1      2
-// 3 : 3     1,1,1.    1,2.  2,1
-// 4 : 5    1,1,1,1   1,1,2    1, 2, 1  , 2,1,1,  2,2
-// 5 : 8     11111   1112    1121  1211 2111 221. 212 122 
-
-// array of length of n, all 1
-// array of length of n - 1, n - 2 worth 1, 1 count of 2
-// n - 2, n - 4 c, 2 count of 2
-
-// if its all two (or if the array of possible step is at 1/2 of the n), we can stop
-//
-//
-//
-
-var climbStairs = function(n, memo = {}) { // 3
-    if (n in memo){
-        return memo[n]
+// 1 => 1
+// 2 => 2
+// 11, 2
+// 3 => 3
+// 111, 12, 21
+// 4 => 5
+// 1111, 112, 121, 211, 22
+// 5 => 8
+// 11111, 1112, 1121, 1211, 2111, 212, 221, 122 
+// 6 => 12
+// 
+var climbStairs = function(n) {
+    let arr = [1, 2] 
+    
+    if(n === 1) return 1
+    if(n === 2) return 2
+    
+    for(let x = 3; x <= n ; x++){ // [1,2,3,5,]
+            let sum = arr[x - 2] + arr[x -3] 
+            arr.push(sum)
     }
     
-    if(n === 1) {
-        return 1
-    }
-
-    if(n === 2) {
-        return 2        
-    }
-    
-    let result = 0
-    
-    result = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)  // func(1) + func (2)
-    memo[n] = result
-    
-    return result
-    
-};
-
-var climbStairs = function(n, memo = {}) { // 3
-    if (n in memo){
-        return memo[n]
-    }
-    if(n === 1) {
-        return 1
-    }
-    if(n === 2) {
-        return 2        
-    }
-        
-    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
-    
-    return memo[n]
-    
+    return arr[n-1]
 };
