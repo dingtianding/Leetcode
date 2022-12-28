8709647a59f66c1ee02c088af52884bfaeb6b07e
Ding Tian Ding <dingtian.ding@gmail.com>
2022-12-27 21:49:52 -0500

    Time: 85 ms (88.57%), Space: 45.6 MB (48.73%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -10,10 +10,25 @@
  * @param {Node|null} root
  * @return {number[]}
  */
-var preorder = function(root, ans=[]) {
-    if (!root) return ans
-    ans.push(root.val)
-    for (let child of root.children)
-        preorder(child, ans)
-    return ans
+// var preorder = function(root, ans=[]) {
+//     if (!root) return ans
+//     ans.push(root.val)
+//     for (let child of root.children)
+//         preorder(child, ans)
+//     return ans
+// };
+
+const preorder = root => {
+    if (!root) return []
+
+    let stack = [root]
+    let result = []
+
+    while (stack.length) {
+        let current = stack.pop();
+        result.push(current.val)
+        if (current.children) stack.push(...current.children.reverse())
+    }
+
+    return result
 };
