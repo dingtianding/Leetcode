170bc1378c251c75b8e66d384a3c80afe2f32803
Ding Tian Ding <dingtian.ding@gmail.com>
2022-12-28 15:41:46 -0500

    Time: 80 ms (78.76%), Space: 46 MB (73.65%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root, low = null, high = null) {
+    if (root===null) return true;
+    
+    if((low != null && root.val <= low)||(high!= null && root.val >= high)){
+        return false;
+    }
+    return isValidBST(root.right, root.val, high) && isValidBST(root.left, low, root.val)
+    
+};
