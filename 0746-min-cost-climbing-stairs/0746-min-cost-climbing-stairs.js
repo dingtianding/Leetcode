1b989005428288a82554b606d056e0146016bfde
Ding Tian Ding <dingtian.ding@gmail.com>
2022-12-29 17:50:38 -0500

    Time: 68 ms (88.06%), Space: 42.5 MB (74.78%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let downOne = 0
+    let downTwo = 0;
+    
+    for(let i = 2; i < cost.length + 1; i++){
+        let temp = downOne
+        downOne = Math.min(downOne + cost[i - 1], downTwo + cost[i - 2]);
+        downTwo = temp;
+    }
+    
+    return downOne
+};
