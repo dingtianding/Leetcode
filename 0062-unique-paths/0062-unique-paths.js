a2f7d345b80e5a4a9b6fc78cd7b7e74af23d2b3a
Ding Tian Ding <dingtian.ding@gmail.com>
2022-12-30 12:22:55 -0500

    Time: 66 ms (86.13%), Space: 42.1 MB (69.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -9,12 +9,23 @@
 //     return uniquePaths(m-1, n) +uniquePaths(m, n-1)
 // };
 
-const uniquePaths = function(m, n, memo = {}) {
-     const key = m + ',' + n;
-     if (key in memo) return memo[key];
-     if (m === 1 || n === 1) return 1
-     if (m === 0 || n === 0) return 0;
+// const uniquePaths = function(m, n, memo = {}) {
+//      const key = m + ',' + n;
+//      if (key in memo) return memo[key];
+//      if (m === 1 || n === 1) return 1
+//      if (m === 0 || n === 0) return 0;
      
-     memo[key] = uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo);
-     return memo[key];
+//      memo[key] = uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo);
+//      return memo[key];
+// };
+var uniquePaths = function(m, n) {
+    let count = Array(m)
+    for(let i=0; i<m; i++)  count[i] = Array(n)
+    for(let i=0; i<m; i++){
+        for(let j=0; j<n; j++){
+            if(i == 0 || j == 0)    count[i][j] = 1
+            else    count[i][j] = count[i][j-1] + count[i-1][j]
+        }
+    }
+    return count[m-1][n-1]
 };
