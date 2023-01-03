351963dc3e30872bd43f99d327d142f8eabf6834
Ding Tian Ding <dingtian.ding@gmail.com>
2023-01-02 21:22:30 -0500

    Time: 103 ms (49.92%), Space: 44.8 MB (17.87%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,23 +3,16 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    if(n < 10){
-        if(n === 1 || n === 7){
-            return true
-        }
-        return false
+    var seen = {};
+    while (n !== 1 && !seen[n]) {
+        seen[n] = true;
+        n = sumOfSquares(n);
     }
-    
-    let total = 0
-    
-    while(n > 0){
-        let sq = n % 10
-        total += sq**2
-        n -= sq
-        n /= 10
-    }
-    if(total === 1){
-        return true
-    }
-    return isHappy(total)
+    return n === 1 ? true : false;
 };
+
+function sumOfSquares(numString) {
+    return numString.toString().split('').reduce(function(sum, num) {
+        return sum + Math.pow(num, 2);
+    }, 0);
+}
