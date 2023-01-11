53a58ee940432e62382e71d734cc62a8535fac85
Ding Tian Ding <dingtian.ding@gmail.com>
2023-01-10 19:39:04 -0500

    Time: 75 ms (60.04%), Space: 42 MB (52.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -10,7 +10,6 @@
 // 4. if its 10 at index of 0, we want to unshift a 1
 var plusOne = function(digits) {
     digits[digits.length - 1] += 1
-
     for(let i = digits.length - 1; i >= 0; i--){   
         if(digits[i] === 10 && i !== 0) {
             digits[i] = 0
