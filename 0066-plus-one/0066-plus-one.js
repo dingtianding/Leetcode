273700092324a0acc299e572e913d6a84d0de7fc
Ding Tian Ding <dingtian.ding@gmail.com>
2023-01-10 19:38:25 -0500

    Time: 71 ms (67.47%), Space: 42.3 MB (16.49%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+// [9,9,9] => [1,0,0,0]
+// 1.get last index
+// 2. increment by one
+// 3. iteration from the back and cascading from back to front.
+// 4. if its 10 at index of 0, we want to unshift a 1
+var plusOne = function(digits) {
+    digits[digits.length - 1] += 1
+
+    for(let i = digits.length - 1; i >= 0; i--){   
+        if(digits[i] === 10 && i !== 0) {
+            digits[i] = 0
+            digits[i - 1]  += 1
+        } else if(digits[i] === 10 && i === 0){
+            digits[i] = 0
+            digits.unshift(1)
+        }   
+    }
+    return digits
+};
