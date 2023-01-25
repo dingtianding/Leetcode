375b3d16dbad4a196a7c160cabd1faf63e179d24
Ding Tian Ding <dingtian.ding@gmail.com>
2023-01-25 16:36:27 -0500

    Time: 70 ms (68.91%), Space: 44.2 MB (42.20%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,16 +9,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+// 3 pointer varibale
+// current, start at head,node 1, 2
+// prev, start at null, 1
+// next, null 2, 3
+// while current != null
+// 
+// be mindful as to if I am return the head of the linked list(previous node of the null node at the end)
 var reverseList = function(head) {
-    let prev = null;
-    let curr = head;
+    let current = head
+    let next = null //
+    let prev = null //
     
-    while(curr) {
-        let nextTemp = curr.next
-        curr.next = prev
-        prev = curr;
-        curr = nextTemp;
+    while(current != null){ // 1, 2,3,4,5, null
+        next = current.next //setting node of 2 aside on a next m,    next= null
+        current.next = prev //having current's pointer to the prev 1 -> null, chaning 2's pointer to the prev variable(1)
+        prev = current //updating the prev variable to to be the current node prev = 5
+        current = next// we are moving current onto next current = 5->null
     }
     return prev
-    
 };
+
