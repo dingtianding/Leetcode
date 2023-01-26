bc182795dcf70961c53e9a485826e76ebbe3efa0
Ding Tian Ding <dingtian.ding@gmail.com>
2023-01-25 22:47:35 -0500

    Time: 72 ms (65.79%), Space: 42.8 MB (44.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -14,19 +14,19 @@
 // ""
 // ["flow","flower","flight"]
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return ""
+    if (!strs.length) return ""
     
     let result = strs[0] // "ab"
     
     for(let i = 1; i < strs.length; i++){//iterate word, "a"
         let str = strs[i] // "a"
-        if (str === "") return ""
+        if (!str) return ""
         
         for(let j = 0; j < result.length; j++){// iterate letter
             let char = str[j] //
             let char2 = result[j] // b of ab
             
-            if(char != char2){ // at index of 2
+            if(char !== char2){ // at index of 2
                 result = result.slice(0, j)// flower -> flow -> fk
                 break
             }
