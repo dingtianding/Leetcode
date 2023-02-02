1c4ad815bd7a9b4501189630375884f866df7333
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-02 18:12:30 -0500

    Time: 81 ms (64.03%), Space: 46 MB (78.40%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,18 +2,39 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums, current = 0, memo = {}) {
-    if(current in memo) return memo[current]
-    if(current >= nums.length - 1) return true
-
-    const maxSteps = nums[current]
+// var canJump = function(nums, current = 0, memo = {}) {
+//     if(current in memo) return memo[current]
+//     if(current >= nums.length - 1) return true
+// 
+//     const maxSteps = nums[current]
         
-    for(let i = 1; i <= maxSteps; i++){
-        if(canJump(nums, current + i, memo)){ 
-        memo[current] = true
-        return true
-        } 
+//     for(let i = 1; i <= maxSteps; i++){
+//         if(canJump(nums, current + i, memo)){ 
+//         memo[current] = true
+//         return true
+//         } 
+//     }
+//     memo[current] = false
+//     return false
+// };
+var canJump = function(nums) {
+  let idx = 0;
+  let max = 0;
+  let target = nums.length - 1;
+
+  while(idx < nums.length) {
+    max = Math.max(max, idx + nums[idx]);
+    
+    if (max >= target) {
+      return true;
     }
-    memo[current] = false
-    return false
+    
+    if (max <= idx && nums[idx] === 0) {
+      return false;
+    }
+    
+    idx++;
+  }
+  
+  return false;
 };
