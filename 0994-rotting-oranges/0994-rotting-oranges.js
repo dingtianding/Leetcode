fe0e32239c2a6d4e854f2600d3c9080822908d14
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-06 23:37:26 -0500

    Time: 78 ms (73.53%), Space: 44.4 MB (72.61%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -10,47 +10,50 @@
 // helper(grid, r, c)
 // set all 4 direction fresh ornage to = 2
 // increase 
-var orangesRotting = function(grid) {
-    let min = 0
-    let done = false
+// que = 
+//
+//
+//
+// var orangesRotting = function(grid) {
+//     let min = 0
+//     let done = false
     
-    while(!done){
-        done = true
-        for(let r = 0; r < grid.length; r++){
-            for(let c = 0; c < grid[0].length; c++){
-                let rotTime = 2 + min// 2 + 1 = 3
-                let pos = grid[r][c] //
+//     while(!done){
+//         done = true
+//         for(let r = 0; r < grid.length; r++){
+//             for(let c = 0; c < grid[0].length; c++){
+//                 let rotTime = 2 + min// 2 + 1 = 3
+//                 let pos = grid[r][c] //
 
-                if(pos === rotTime){//right,down, left, up
-                    
-                    if(c + 1 < grid[0].length && grid[r][c+1]===1) {
-                        grid[r][c+1] = rotTime + 1 // 4
-                        done = false
-                    }
-                    if(r + 1 < grid.length && grid[r+1][c]===1) {
-                        grid[r+1][c] = rotTime + 1
-                        done = false
-                    }
-                    if(0 <= c - 1 && grid[r][c-1]===1) {
-                        grid[r][c-1] = rotTime + 1
-                        done = false
-                    }
-                    if(0 <= r - 1 && grid[r-1][c] ===1) {
-                        grid[r-1][c] = rotTime + 1
-                        done = false
-                    }
-                }
-            }    
-        }
-        min++ //1
-    }
-    for(let r = 0; r < grid.length; r++){
-        for(let c = 0; c < grid[0].length; c++){
-            if(grid[r][c]===1) return -1
-        }
-    }
-    return min - 1
-};
+//                 if(pos === rotTime){//right,down, left, up
+//                     if(c + 1 < grid[0].length && grid[r][c+1]===1) {
+//                         grid[r][c+1] = rotTime + 1 // 4
+//                         done = false
+//                     }
+//                     if(r + 1 < grid.length && grid[r+1][c]===1) {
+//                         grid[r+1][c] = rotTime + 1
+//                         done = false
+//                     }
+//                     if(0 <= c - 1 && grid[r][c-1]===1) {
+//                         grid[r][c-1] = rotTime + 1
+//                         done = false
+//                     }
+//                     if(0 <= r - 1 && grid[r-1][c] ===1) {
+//                         grid[r-1][c] = rotTime + 1
+//                         done = false
+//                     }
+//                 }
+//             }    
+//         }
+//         min++
+//     }
+//     for(let r = 0; r < grid.length; r++){
+//         for(let c = 0; c < grid[0].length; c++){
+//             if(grid[r][c]===1) return -1
+//         }
+//     }
+//     return min - 1
+// };
 
 
 //[[2,1,1],
@@ -65,3 +68,58 @@ var orangesRotting = function(grid) {
 // return min
 // [[0,2]] = 0 already done
 // run M(r) * N(c) * Min(# of min)
+var orangesRotting = function(grid) {
+    const que = []
+    let total = 0
+    let rotten = 0
+    
+    for(let r = 0; r < grid.length; r++){
+             for(let c = 0; c < grid[0].length; c++){
+                 if(grid[r][c]===2){
+                     que.push([r,c])
+                     rotten++
+                     total++
+                 } else if(grid[r][c]===1){
+                     total++
+                 }
+        }
+    }
+    
+    if(total===0) return 0
+    
+    let min = 0
+    while(que.length){
+        let size = que.length
+        while(size){
+            size--
+            const [r, c] = que.shift() //[r, c]
+            
+            let rotTime = 2 + min// 2 + 1 = 3
+            let pos = grid[r][c] //
+
+            if(c + 1 < grid[0].length && grid[r][c+1]===1) {
+                grid[r][c+1] = rotTime + 1 // 4
+                que.push([r,c+1])
+                rotten++
+            }
+            if(r + 1 < grid.length && grid[r+1][c]===1) {
+                grid[r+1][c] = rotTime + 1
+                que.push([r+1,c])
+                rotten++
+            }
+            if(0 <= c - 1 && grid[r][c-1]===1) {
+                grid[r][c-1] = rotTime + 1
+                que.push([r,c-1])
+                rotten++
+            }
+            if(0 <= r - 1 && grid[r-1][c] ===1) {
+                grid[r-1][c] = rotTime + 1
+                que.push([r-1,c])
+                rotten++
+            }
+        }
+        min++
+    }
+    return rotten === total ? min - 1 : -1
+    
+};
