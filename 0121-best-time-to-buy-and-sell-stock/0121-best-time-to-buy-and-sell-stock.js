014fd6bac25fbb6be17105093d761fa76eeb2768
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-10 11:23:32 -0500

    Time: 97 ms (43.61%), Space: 51.4 MB (83.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,15 +2,30 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// input: array of stock price, 
+// day factor: price can be eariler or later
+// profit = sell day/price - buy day/price 
+// postive or return profit 
+//
+// 1. set maxprofit update if higher exist , = 0
+// 2. set bestcost update if better cost, = prices[0]
+// 3. iterate through price history and recalc cur profit starting from second
+// 4. max profit
+// 
+// time of N
+// space of 2, 1
 var maxProfit = function(prices) {
-    if(prices == null || prices.length <= 1) return 0;
+    let maxP = 0
+    let minC = prices[0] // 7
     
-    let minBuy = prices[0];
-    let profit = 0;
-    
-    for(let i = 1; i < prices.length; i++) {
-        minBuy = Math.min(minBuy, prices[i]);
-        profit = Math.max(profit, prices[i] - minBuy);
+    for(let i = 1; i < prices.length; i++){
+        let current = prices[i] // 6
+        let profit = current - minC // 6 - 1 = 5
+                
+        minC = Math.min(minC, current) // cost = 7,6,4,3,1
+        maxP = Math.max(maxP, profit) // 0 > negative
     }
-    return profit;
+    
+    return maxP // 0
 };
