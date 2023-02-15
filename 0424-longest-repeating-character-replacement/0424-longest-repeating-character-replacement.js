55ac26935b5ac6a3359559ee68b90a2aa1642bb3
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-15 18:52:40 -0500

    Time: 78 ms (89.06%), Space: 42.5 MB (73.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -3,58 +3,32 @@
  * @param {number} k
  * @return {number}
  */
-function characterReplacement(s, k) {
-    // binary search over the length of substring
-    // lo contains the valid value, and hi contains the
-    // invalid value
-    let lo = 1,
-        hi = s.length + 1;
-    while (lo + 1 < hi) {
-        let mid = lo + Math.floor((hi - lo) / 2);
+// "ABAB" 2 times = "AAAA" or "BBBB" = 4char
+// "AABABBA" 1 times = "AAAABBA" or AABBBBA" = 4 char
 
-        // can we make a valid substring of length `mid`?
-        if (canMakeValidSubstring(s, mid, k)) {
-            // explore the right half
-            lo = mid;
-        } else {
-            // explore the left half
-            hi = mid;
+// Time Complexity :  O(n)
+// Space Complexity : O(1)
+var characterReplacement = function(s, k) {
+    // Make a map of size 26...
+    var map = [26]
+    // Initialize largestCount, maxlen & beg pointer...
+    let largestCount = 0, beg = 0, maxlen = 0;
+    // Traverse all characters through the loop...
+    for(let end = 0; end < s.length; end++){
+        const c = s[end]
+        map[c] = (map[c] || 0) + 1
+        // Get the largest count of a single, unique character in the current window...
+        largestCount = Math.max(largestCount, map[c])
+        // We are allowed to have at most k replacements in the window...
+        // So, if max character frequency + distance between beg and end is greater than k...
+        // this means we have considered changing more than k charactres. So time to shrink window...
+        // Then there are more characters in the window than we can replace, and we need to shrink the window...
+        if(end - beg + 1 - largestCount > k){     // The main equation is: end - beg + 1 - largestCount...
+            map[s[beg]] -= 1
+            beg += 1
         }
+        // Get the maximum length of repeating character...
+        maxlen = Math.max(maxlen, end - beg + 1);     // end - beg + 1 = size of the current window...
     }
-
-    // length of the longest substring that satisfies
-    // the given condition
-    return lo;
-}
-
-function canMakeValidSubstring(s, substringLength, k) {
-    // take a window of length `substringLength` on the given
-    // string, and move it from left to right. If this window
-    // satisfies the condition of a valid string, then we return
-    // true
-    let freqMap = {};
-    let maxFrequency = 0;
-    let start = 0;
-    for (let end = 0; end < s.length; end += 1) {
-        freqMap[s[end]] ||= 0;
-        freqMap[s[end]] += 1;
-
-        // if the window [start, end] exceeds substringLength
-        // then move the start pointer one step toward right
-        if (end + 1 - start > substringLength) {
-            // before moving the pointer toward right, decrease
-            // the frequency of the corresponding character
-            freqMap[s[start]] -= 1;
-            start += 1;
-        }
-
-        // record the maximum frequency seen so far
-        maxFrequency = Math.max(maxFrequency, freqMap[s[end]]);
-        if (substringLength - maxFrequency <= k) {
-            return true;
-        }
-    }
-
-    // we didn't a valid substring of the given size
-    return false;
-}
+    return maxlen;      // Return the maximum length of repeating character...
+};
