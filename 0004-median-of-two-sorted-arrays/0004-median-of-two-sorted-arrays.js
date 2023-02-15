9f7f5b852bb8dd86b3e4cf80afe5bff084b3428b
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-14 20:58:54 -0500

    Time: 115 ms (51.95%), Space: 45.8 MB (96.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -5,25 +5,24 @@
  */
 var findMedianSortedArrays = function(nums1, nums2) {
     if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1)
-    let x = nums1.length
-    let y = nums2.length
-    let low = 0, high = x
-    while(low <= high) {
-        const midX = (high + low) >> 1
-        const midY = ((x + y + 1) >> 1) - midX
+    let x = nums1.length // 2
+    let y = nums2.length // 2
+    let low = 0, high = x // 0 , 2
+    while(low <= high) {// 1 2
+        const midX = (high + low) >> 1 // 1.5
+        const midY = ((x + y + 1) >> 1) - midX // 2.5 - 1.5 = 1 = 1
         
-        const maxX = midX == 0 ? -Infinity : nums1[midX - 1]
-        const maxY = midY == 0 ? -Infinity : nums2[midY - 1]
+        const maxX = midX == 0 ? -Infinity : nums1[midX - 1] // 1
+        const minX = midX == nums1.length ? Infinity : nums1[midX] // 2
+        const maxY = midY == 0 ? -Infinity : nums2[midY - 1] // 3
+        const minY = midY == nums2.length ? Infinity : nums2[midY] //4
         
-        const minX = midX == nums1.length ? Infinity : nums1[midX]
-        const minY = midY == nums2.length ? Infinity : nums2[midY]
-        
-        if(maxX <= minY && maxY <= minX) {
+        if(maxX <= minY && maxY <= minX) { // 1 < 4 and 3 < 2
             const lowMax = Math.max(maxX, maxY)
             if( (x + y) % 2 == 1)
                 return lowMax
             return (lowMax + Math.min(minX, minY)) / 2
-        } else if(maxX < minY) {
+        } else if(maxX < minY) { // 1 < 4
             low = midX + 1
         } else if(maxX > minY)
             high = midX - 1
