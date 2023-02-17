03976ef2a2b58c23ad17a34a0839a509204902e8
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-17 13:59:55 -0500

    Time: 62 ms (95.93%), Space: 44.1 MB (69.47%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -2,38 +2,31 @@
  * @param {number[][]} grid
  * @return {number[]}
  */
-var findBall = function(grid) 
-{
-    const rows = grid.length, cols = grid[0].length;
-
-    const getPosition = function(row, col)
-    {
-        if(grid[row][col] === 1) // if to left
-        {
-            if(col === cols-1 || grid[row][col+1] === -1)// if the one to left is opp or at the end
-                return -1;
-            return col+1;
-        }
-        else
-        {
-            if(col === 0 || grid[row][col-1] === 1) // opposite
-                return -1;
-            return col-1;
-        }
-    };
-
-    let result = [];
-    for(let col=0; col<cols; col++)
-    {
-        let pos = col;
-        for(let row=0; row<rows; row++)
-        {
-            pos = getPosition(row, pos);
-            if(pos === -1)
-                break;
-        }
-        result.push(pos);
-    }
+// M * N
+var findBall = function(grid) {
+    const rows = grid.length, cols = grid[0].length
     
-    return result;
+    
+    const getCol = function(row, col){// row 0, col 1
+        if(grid[row][col] === 1){//if cell is right facing
+            if(col === cols-1 || grid[row][col+1]=== -1) return -1;
+            return col+1 // //row 0, col2
+        }
+        else if (grid[row][col] === -1) {// b3
+            if(col === 0 || grid[row][col-1]=== 1) return -1;
+            return col-1
+        } 
+    } 
+    let result = []
+    
+    for(let col = 0; col< cols; col++){// ball
+        let pos = col; // ball ending pos = [0,1,2,3,4]
+        for(let row = 0; row < rows; row++){//row 0,1
+            pos = getCol(row, pos);//use DFS to find the final position
+            if(pos === -1)
+                break
+        }
+        result.push(pos)
+    }
+    return result
 };
