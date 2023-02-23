562e1ba2e28bf04e204142b6fbb74e174f3b10e1
Ding Tian Ding <dingtian.ding@gmail.com>
2023-02-22 19:32:50 -0500

    Time: 446 ms (5.05%), Space: 63.6 MB (5.14%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,62 @@
+
+class MinStack {
+    constructor(){
+        this.stack = []
+        this.mins = []
+    }
+};
+// [ -2,0,-3]  top looks here
+// [ -2,-2,-3] get min looks here
+
+
+// ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+// [        [],   [6],   [6],   [7],   [],   [],      [],   [],      [],   [],   [7],   [],      [],  [-8],   [],      [],   [],      []]
+//                                      7.    7        6.    6.       6.    6.    7.     7.      7       
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    
+    let min = this.mins[this.mins.length - 1] // last min in the mins array
+     
+    if(val < min || this.mins.length === 0){
+        this.mins.push(val)
+    } else {
+        this.mins.push(min)
+    }
+    console.log(this.mins)
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.mins.pop();
+
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.mins[this.mins.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
