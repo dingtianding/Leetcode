cb0c7d93c6b68574ec4eb6f672cfb416deca1360
Ding Tian Ding <dingtian.ding@gmail.com>
2023-03-20 22:49:22 -0400

    Time: 56 ms (85.14%), Space: 41.9 MB (64.35%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -7,7 +7,7 @@
 //      l m r  
 // find min num
 // [3,4,5,0,1,1.5, 2] 
-//      l,m,r         
+//        lr         
 // 
 // [4,5,6,7,0,1,2] 
 //        lm,r    
@@ -28,16 +28,34 @@ var findMin = function(nums) {
     
     while(l < r){
         let mid = Math.floor(l / 2 + r / 2)
-        
-        if(mid === l || mid === r) return Math.min(nums[l],nums[r])
-        
+                
         if(nums[mid] > nums[r]){
-            l = mid
-        } else if(nums[mid] > nums[l]){
+            l = mid + 1
+        } else {
             r = mid
-        } else { //mid is smaller to both l + r number
-            r = mid
-        }
+        } 
     }
     return nums[l]
 };
+
+// var findMin = function(nums) {
+//   // 找最小其实就是找rotate的点
+//   let left = 0
+//   let right = nums.length - 1
+  
+//   if (nums[left] < nums[right]) return nums[left]
+    
+//   while (left < right) {
+//     let mid = parseInt(left + (right - left) / 2)
+    
+//     if (nums[mid] > nums[right]) {
+//       // mid在大区间一侧
+//       left = mid + 1
+//     } else {
+//       // 一直往小的区间逼近
+//       right = mid
+//     }
+//   }
+//   // rotate的点
+//   return nums[left]
+// };
