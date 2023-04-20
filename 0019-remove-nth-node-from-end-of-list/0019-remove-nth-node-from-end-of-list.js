89474ba5a75e7e89f13e78c02d721122b902a079
Ding Tian Ding <dingtian.ding@gmail.com>
2023-04-19 20:11:43 -0400

    Time: 67 ms (35.90%), Space: 43.1 MB (58.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -13,11 +13,11 @@
 var removeNthFromEnd = function(head, n) {
     let fast = head, slow = head
     
-    for (let i = 0; i < n; i++) fast = fast.next
+    for(let i = 0; i < n; i++) fast = fast.next
     
-    if (!fast) return head.next
+    if(!fast) return head.next
     
-    while (fast.next) fast = fast.next, slow = slow.next
+    while(fast.next)  fast = fast.next, slow = slow.next;
     
     slow.next = slow.next.next
     
