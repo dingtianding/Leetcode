56b1f007b32dfa60d001becbc786a63480972b1b
Ding Tian Ding <dingtian.ding@gmail.com>
2023-05-11 18:14:35 -0400

    Time: 73 ms (66.83%), Space: 47.3 MB (56.17%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -16,12 +16,13 @@ var isBalanced = function(root) {
     
     let dfs = function(node) {
         if(!node) return 0;
-        let left = 1 + dfs(node.left);
-        let right = 1 + dfs(node.right);
+        
+        let left = dfs(node.left);
+        let right = dfs(node.right);
         
         if(Math.abs(left - right) > 1) return Infinity;
         
-        return Math.max(left, right);
+        return 1 + Math.max(left, right);
     }
     
     return dfs(root) == Infinity ? false: true;
