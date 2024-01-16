46a82620bcecc8e42eab29379cdf9f29d4ba36f6
Ding Tian Ding <dingtian.ding@gmail.com>
2024-01-16 17:10:33 -0500

    Time: 35 ms (90.73%), Space: 17.5 MB (23.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,20 +1,37 @@
+#Understand
+#Match
+#Plan
+#Implement
+#Review
+#Evaluate Big O
+#
+#Situation
+#Task   
+#Approach
+#Result(Quantify)
+#
+# sort the nums
+# if statement that checks duplicate as we interate through number
+# if there is a duplicate, continue
+# n ^ t/m + 1
+# n * n ^ depth 
+# 1
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]: 
         res = []
-        nums.sort()
+        nums.sort() #[1,2,2]
         
         def backtrack(i, subset):
-            if i == len(nums):
-                res.append(subset.copy())
+            if i == len(nums): 
+                res.append(subset[::])
                 return
-            subset.append(nums[i])
-            backtrack(i + 1, subset)
-            subset.pop()
-            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            subset.append(nums[i]) 
+            backtrack(i + 1, subset) # [1,2,2]
+            
+            subset.pop() #[1,2]
+            while  i + 1 < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
             backtrack(i + 1, subset)
-            
-            
-        backtrack(0,[])
-        return res
         
+        backtrack(0, [])
+        return res
