d1b556b556e5fb7f997bcb74ebc364d1565c0a8f
Ding Tian Ding <dingtian.ding@gmail.com>
2024-11-13 23:21:27 -0500

    Time: 13 ms (66.62%), Space: 12.2 MB (79.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,7 +7,7 @@ class Solution(object):
         i, j = 0, len(s) - 1
         
         while i < j:
-            while i < j and not s[i].isalnum():
+            while i < j and not s[i].isalnum(): #keep on skipping so we have to keep track of the pointers locations
                 i += 1
             while i < j and not s[j].isalnum():
                 j -= 1
