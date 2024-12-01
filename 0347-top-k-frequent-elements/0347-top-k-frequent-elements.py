2debb7a2c3cb09027624fcd41e919c9273ce9655
Ding Tian Ding <dingtian.ding@gmail.com>
2024-12-01 01:29:44 -0500

    Time: 7 ms (80.64%), Space: 13.9 MB (57.51%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+            
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+                
+        res = []
+        
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+            
+        return res
