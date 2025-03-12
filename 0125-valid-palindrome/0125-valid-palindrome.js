ba41324a33fc3c8ff45eb16b0e07318893857335
Ding Tian Ding <dingtian.ding@gmail.com>
2025-03-12 07:57:24 -0400

    Time: 7 ms (36.14%), Space: 57.6 MB (25.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,46 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-
-
-// string 
-// 1. convert upcase to lowcase 
-// 2. remove all non-alphanumeric
-// 3. removes space
-
-// helper method get my string to convert 
-// main check if it is palindrome
-
-// T 2*N
-// S N
-// abcba
 var isPalindrome = function(s) {
-    let converted = convert(s) // "amanaplanacanalpanama"
-    if (!converted.length) return true
-    
-    p1 = 0
-    p2 = converted.length - 1
-    while(p1 < p2){
-        if(p1===p2) return true
-        
-        let char1 = converted[p1]// c
-        let char2 = converted[p2]// c
-        
-        if(char1===char2){
-            p1++
-            p2--
-        } else{
-            return false
+    s = s.toLowerCase().replace(/[^a-z0-9]/g,'');
+    let left = 0
+    let right = s.length -1;
+
+    while(left < right){
+        if(s[left]!==s[right]){
+            return false;
         }
+        left++;
+        right--;
     }
     return true
-    
 };
-
-var convert = function(s){
-    let lowercase = s.toLowerCase()
-    let converted = lowercase.replace(/[^0-9a-z]/gi, '')
-    
-    return converted
-}
-
