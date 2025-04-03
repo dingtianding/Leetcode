98e1e664f0a152956df400810360a0137a9513e0
Ding Tian Ding <dingtian.ding@gmail.com>
2025-04-02 20:05:01 -0400

    Time: 221 ms (8.78%), Space: 51.4 MB (34.92%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,32 @@
+# [10,16],[2,8],[1,6],[7,12]]
+# 11111
+#    22222
+# 11112222
+#          333333
+#              44444  
+# sort it
+# going thoruhg use start2 < end1 to find overlap
+# if true
+#we merged: by taking the start1, greater of end1&2
+# once we finish merge, we return the len(of merged array)
+# N * log N
+# N 
+#[[1, 6],
+#   [2, 8], 
+#      [7, 12], 
+#         [10, 16]]
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        merged = []
+        for point in points:
+            if not merged or merged[-1][1] < point[0]:
+                merged.append(point)
+            else:
+                merged[-1][0] = max(merged[-1][0],point[0])
+                merged[-1][1] = min(merged[-1][1],point[1])
+
+        print(merged)
+        return len(merged)
