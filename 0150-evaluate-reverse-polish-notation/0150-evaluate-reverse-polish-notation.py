b6c3933260d0bc26d91597fbf7dd78e7b4eddaec
Ding Tian Ding <dingtian.ding@gmail.com>
2025-04-04 20:48:36 -0400

    Time: 2 ms (72.6%), Space: 19.1 MB (88.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,65 @@
+# stack = result, 3
+# + operation result = stack[-1] + stack[-1] 
+# * operation = result * 3 = 
+# put it back
+# round down
+# stack to keep track of past values and result
+# when we hit any kind of operation, we going access last two position and calc according
+# push the result back into stack
+# assuming 1 result in the stack return that 
+# performance = N
+# space = N 
+# 
+# 
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        
-        for c in tokens:
-    
-            if c ==  "+":
-                sum = stack.pop() + stack.pop()
-                stack.append(sum)
-            elif c == "-":
-                a, b = stack.pop(), stack.pop()
-                diff = b - a
-                stack.append(diff)
-            elif c == "*":
-                prod = stack.pop() * stack.pop()
-                stack.append(prod)
-            elif c == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(float(b) / a))
-            else:
-                stack.append(int(c))
+
+        for token in tokens:
+            if token == "+":
+                second = stack.pop()
+                first = stack.pop()
+                result = first + second
+            elif token == "-":
+                second = stack.pop()
+                first = stack.pop()
+                result = first - second
+            elif token == "*":
+                second = stack.pop()
+                first = stack.pop()
+                result = first * second
+            elif token == "/":
+                second = stack.pop()
+                first = stack.pop()
+                result = first / second
+                if result > 0:
+                    result = math.floor(result)
+                else:
+                    result = math.ceil(result)        
+            else: 
+                result = int(token)
+            stack.append(result)
+            # print(stack)
+
         return stack[0]
-    
+
+        # tokens =
+# ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+# Use Testcase
+# Stdout
+# [10]
+# [10, 6]
+# [10, 6, 9]
+# [10, 6, 9, 3]
+# [10, 6, 12] + 
+# [10, 6, 12, -11] *
+# [10, 6, -132]  
+# [10, -1]
+# [-10]
+# [-10, 17]
+# [7]
+# [7, 5]
+# [12
+        
