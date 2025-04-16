5097977da98c19346a9340d59a9af70041ca6998
Ding Tian Ding <dingtian.ding@gmail.com>
2025-04-16 00:58:25 -0400

    Time: 0 ms (100%), Space: 18 MB (18.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,35 +3,48 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# iterate through both list
-# compare
-# if one is smaller we append it to a dummy head
-# if one of the list reach null, we append the rest of the other list
 #
-# N
-# 1
+# two pointer
+# p1 on list1.val
+# p2 on list2.val
+# while p1 and p2 on valid number
+# if val1 > val2:
+# add val2 to new list
+# list2 = list2.next
+# else
+# add val1 to new list
+# next
+# out of while
+# if to see which one still have valid val:
+# we add the val to new list until we run out
+# return head of new list
+# 
+# M + N
+# 1 or N 
+# result array reutnr whole arry = 1
+# linkedin return head of the linked = N
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        tail = head
-        cur1 = list1
-        cur2 = list2
+        result = ListNode()
+        tail = result
+        p1  = list1
+        p2  = list2
         
-        while cur1 and cur2:
-            if cur1.val < cur2.val:
-                tail.next = cur1
-                tail = tail.next
-                cur1 = cur1.next
+        while p1 and p2:
+            if p1.val > p2.val:
+                tail.next = p2
+                p2 = p2.next
             else:
-                tail.next = cur2
-                tail = tail.next
-                cur2 = cur2.next
-        
-        if cur1:
-            tail.next = cur1
+                tail.next = p1
+                p1 = p1.next
+            tail = tail.next
+
+        if p1:
+            tail.next = p1
         else:
-            tail.next = cur2
-            
-        return head.next
+            tail.next = p2
+
+        return result.next
+
         
