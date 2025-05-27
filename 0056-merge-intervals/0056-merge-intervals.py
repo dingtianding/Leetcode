9fa7c5fd3539f0c71f4f1896eca766798773e513
Ding Tian Ding <dingtian.ding@gmail.com>
2025-05-27 03:41:15 -0400

    Time: 8 ms (48.77%), Space: 21.6 MB (29.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,36 @@
+# sort array based on first position, 
+# definte mergeable:
+# result = [[1,6][8,10],[15,18]]
+# 
+# start2 is starting before end1(if they equal, they are also mergable). Before first interval is properly end, 2nd already interval
+# if merageble:
+#      merged = [lesser of starts, greater ends]
+#       get a new 2nd
+# push the 1st into result,
+# 2nd one -> new 1st one
+# time  N * logN
+# space 1
+#
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
+        # if len(intervals) == 1:
+        #     return intervals
 
-        res = []
+        intervals.sort() # sort returns null
+        result = []
+        start1, end1 = intervals[0]# 1,3
 
-        for interval in intervals:
-            if not res or res[-1][1] < interval[0]:
-                res.append(interval)
+        for i in range(1, len(intervals)):
+            start2, end2 = intervals[i] # 15,18
+            if start2 <= end1: # 8 < 6 == merageable
+                end1 = max(end1, end2) # 3 < 6 = 6
+                #[1,6]
             else:
-                res[-1][1] = max(res[-1][1],interval[1])
+                result.append([start1, end1]) #result=[1,6],[8,10]
+                start1 = start2 # [15,18]
+                end1 = end2
 
-        return res
+        result.append([start1,end1])
+
+        return result
         
